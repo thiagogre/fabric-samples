48ba7117380dc3b580c1b82b8f34d95c4b52ae17
thiagogre <thiagogre.dev@gmail.com>
2024-05-27 18:56:38 -0300

    add ca flag in the test network

--- test-network/run-test-network.sh
+++ test-network/run-test-network.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./network.sh down && ./network.sh up createChannel -s couchdb && cd addOrg3 && ./addOrg3.sh up -s couchdb && cd .. && ./network.sh deployCC -ccn basic -ccp ../asset-transfer-basic/chaincode-go -ccl go
+./network.sh down && ./network.sh up createChannel -ca -s couchdb && cd addOrg3 && ./addOrg3.sh up -ca -s couchdb && cd .. && ./network.sh deployCC -ccn basic -ccp ../asset-transfer-basic/chaincode-go -ccl go
